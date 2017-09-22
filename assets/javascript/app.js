62a56892c1dbfcc20cb1074d2e37227f108bba77
Gavin Clay <cgavin40@yahoo.com>
2017-09-22 08:07:52 -0400

    merge

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,17 @@
  /*added database contents --- wenfang */
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyD6f-EgnKwmX6Ff31ti0ObrYiCwAb1TNrI",
     authDomain: "my-firebase-project-d7eb2.firebaseapp.com",
     databaseURL: "https://my-firebase-project-d7eb2.firebaseio.com",
     projectId: "my-firebase-project-d7eb2",
     storageBucket: "my-firebase-project-d7eb2.appspot.com",
     messagingSenderId: "368788756012"
- };
- firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
- var database = firebase.database();
- 
+var database = firebase.database();
+
 // $(document).ready(function() {
 // //displays modal
 // $('#myModal').modal('show');
@@ -32,7 +32,7 @@ database.ref("Posts").on("child_added", function(snap) {
     cityOptions.push(cityString);
     //added a search counter property to each city
     cityRef.once("value", function(citysnap) {
-        if(!citysnap.child("srchCounter").exists()) { 
+        if (!citysnap.child("srchCounter").exists()) {
             var srchCount = 0;
             cityRef.child("srchCounter").set(srchCount);
         }
@@ -44,153 +44,151 @@ database.ref("Posts").on("child_added", function(snap) {
 // autocomplete is not a function?
 $("#where").autocomplete({
 
-$(".container-fluid").hide(); // Mark addded conatiner-fluid hide to stop text runnoff
- 
-$(".where").autocomplete({
-  source: cityOptions
-});
+            $(".container-fluid").hide(); // Mark addded conatiner-fluid hide to stop text runnoff
 
-var int;  //changed the interval variable scope to global  -- wenfang
-var srchResPage = false; //a flag for checking if current 'page' is search page or result page
+            $(".where").autocomplete({
+                source: cityOptions
+            });
 
-    // on click function
-    $(".submit").on("click" , function(event) {
-        event.preventDefault();
-        $(".container-fluid").show(); // Mark added container-fluid show rule on submit
- 
+            var int; //changed the interval variable scope to global  -- wenfang
+            var srchResPage = false; //a flag for checking if current 'page' is search page or result page
 
-        imageArray = [];  //reset imageArray
-        srchResPage = true;
+            // on click function
+            $(".submit").on("click", function(event) {
+                event.preventDefault();
+                $(".container-fluid").show(); // Mark added container-fluid show rule on submit
 
-        // take value from input name
-        var cityInput = $(".where").val().trim();  //To agree with the database, city input from the 'where' search box has the form 'New York City, United States' -- wenfang
-        var arr = cityInput.split(",");
-        cityName = arr[0];
-        console.log("cityName var is: " + cityName);
 
-        // hides the search box
-        $(".wrap").hide();
+                imageArray = []; //reset imageArray
+                srchResPage = true;
 
-        // Getty image apikey
-        var gettyApiKey = 'd6e3snbs3ch2qvk8wctwnjmc';
-        // Getty image ajax call
-        $.ajax({
-            type:'GET',
-            url:"https://api.gettyimages.com/v3/search/images/creative?fields=display_set&phrase=" + cityName,
-            beforeSend: function (request) {
-            request.setRequestHeader("Api-Key", gettyApiKey);
-        }}).done(function(data) {
+                // take value from input name
+                var cityInput = $(".where").val().trim(); //To agree with the database, city input from the 'where' search box has the form 'New York City, United States' -- wenfang
+                var arr = cityInput.split(",");
+                cityName = arr[0];
+                console.log("cityName var is: " + cityName);
 
-            console.log("Ran ajax success");
-            $(".panel-title").html("<strong>" + cityName + "</strong>");
-            // loop through images on Getty images
-            for(var i = 0; i < data.images.length; i++) {
-                // push images into an array - this would be used if we wanted to loop a certain number of images instead all of them
-                imageArray.push(data.images[i].display_sizes[0].uri);
-                // $(".cityImage").html("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
-            }
-            // console.log(imageArray);
-            var i = 0;
-            $(".cityImage").show();
-            changeImg();  //call for the first time 
+                // hides the search box
+                $(".wrap").hide();
 
-            // set loop for X seconds
-            int = self.setInterval(changeImg, 1000 * 5);
-            //a back-to-search button added
-            var backBtn = $("<button class='submit backbutton'>").text("Back to Search");
-            $(".cityImage").append(backBtn);
+                // Getty image apikey
+                var gettyApiKey = 'd6e3snbs3ch2qvk8wctwnjmc';
+                // Getty image ajax call
+                $.ajax({
+                    type: 'GET',
+                    url: "https://api.gettyimages.com/v3/search/images/creative?fields=display_set&phrase=" + cityName,
+                    beforeSend: function(request) {
+                        request.setRequestHeader("Api-Key", gettyApiKey);
+                    }
+                }).done(function(data) {
 
-            // displays images in DOM and increments i
-            function changeImg() {
-                // $(".cityImage").html("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
-                // $(".cityImage").html("<img src='" + imageArray[i] + "'/>");
-                $(".cityImage img").attr("src", imageArray[i]);
-                i++;
-                if(i===imageArray.length)  //need to reset i to 0  -- wenfang
-                    i=0;
-            }
-        });
+                    console.log("Ran ajax success");
+                    $(".panel-title").html("<strong>" + cityName + "</strong>");
+                    // loop through images on Getty images
+                    for (var i = 0; i < data.images.length; i++) {
+                        // push images into an array - this would be used if we wanted to loop a certain number of images instead all of them
+                        imageArray.push(data.images[i].display_sizes[0].uri);
+                        // $(".cityImage").html("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
+                    }
+                    // console.log(imageArray);
+                    var i = 0;
+                    $(".cityImage").show();
+                    changeImg(); //call for the first time 
 
-        // World Weather Online apikey
-        $(".cityWeather").show();
-        $("#localTimeWeather").empty();
-        var wwoApiKey = 'ac4c2f19b48740fd9ce201023171209';
-        var weatherURL = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=" + cityName + "&format=json&key=" + wwoApiKey;
-        var tzURL = "http://api.worldweatheronline.com/premium/v1/tz.ashx?q=" + cityName + "&format=json&key=" + wwoApiKey;
-        $.ajax({
-            url: tzURL,
-            method: "GET"
-        }).done(function(localtime) {
-            $("#localTimeWeather").prepend("Local Time:" + "\xa0\xa0\xa0\xa0" + localtime.data.time_zone[0].localtime + "<br/>");
-        });
+                    // set loop for X seconds
+                    int = self.setInterval(changeImg, 1000 * 5);
+                    //a back-to-search button added
+                    var backBtn = $("<button class='submit backbutton'>").text("Back to Search");
+                    $(".cityImage").append(backBtn);
 
-        $.ajax({
-            url: weatherURL,
-            method: "GET"
-        }).done(function(localweather) {
-            $("#localTimeWeather").append("Current Weather: " + localweather.data.current_condition[0].weatherDesc[0].value + 
-                                        "\xa0\xa0\xa0\xa0\xa0\xa0" + "Temperature (F): " + localweather.data.current_condition[0].temp_F +
-                                        "\xa0\xa0\xa0\xa0\xa0\xa0" + "Wind Speed (Mph): " + localweather.data.current_condition[0].windspeedMiles +
-                                        "\xa0\xa0\xa0\xa0\xa0\xa0" + "Humidity: " + localweather.data.current_condition[0].humidity);
-            var minTemp = "<tr><td>Avg Min Temp(F)<td>";
-            var maxTemp = "<tr><td>Abs Max Temp(F)<td>";
-            var rainFall = "<tr><td>Avg Daily Rainfall(in)<td>";
-            for (var i=0; i<12; i++) {
-                if(i===11) {
-                    minTemp += localweather.data.ClimateAverages[0].month[i].avgMinTemp_F + "</td></tr>";
-                    maxTemp += localweather.data.ClimateAverages[0].month[i].absMaxTemp_F + "</td></tr>";
-                    rainFall += localweather.data.ClimateAverages[0].month[i].avgDailyRainfall + "</td></tr>";
-                }
-                else {
-                    minTemp += localweather.data.ClimateAverages[0].month[i].avgMinTemp_F + "</td><td>";
-                    maxTemp += localweather.data.ClimateAverages[0].month[i].absMaxTemp_F + "</td><td>";
-                    rainFall += localweather.data.ClimateAverages[0].month[i].avgDailyRainfall + "</td><td>";
-                }
-            }
-            $("#weather-table > tbody").append(minTemp);
-            $("#weather-table > tbody").append(maxTemp);
-            $("#weather-table > tbody").append(rainFall);
-        });
-        
-        /* Added codes for importing things-to-do contents to the cityToDo div -- wenfang */
-        $(".popularSrches").empty();
-        $(".cityToDo h2").text("Things To Do in " + cityName);
-        $(".cityToDo").append("<hr>");
-        // database.ref("Posts").on("value", function(snapshot) {
-        database.ref("Posts").once("value", function(snapshot) {
-            if(snapshot.child(cityInput).exists()) {
-                var childRef = snapshot.ref.child(cityInput);
-                childRef.once("value", function(childsnap) {
-                    childsnap.forEach(function(snap){
-                        var key = snap.key;
-                        if(key.includes("Paragraph")) {
-                            var paragraphDiv = $("<div>").html("<p class='lead'>" + snap.val() + "</p>");
-                            $(".cityToDo").append(paragraphDiv);
-                        }
-                        else if(key === "srchCounter") {  //increment the city's search count
-                            var cnt = snap.val()+1;
-                            childRef.child("srchCounter").set(cnt);
-                        }
-                    });
+                    // displays images in DOM and increments i
+                    function changeImg() {
+                        // $(".cityImage").html("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
+                        // $(".cityImage").html("<img src='" + imageArray[i] + "'/>");
+                        $(".cityImage img").attr("src", imageArray[i]);
+                        i++;
+                        if (i === imageArray.length) //need to reset i to 0  -- wenfang
+                            i = 0;
+                    }
                 });
-            }
-        }, function(errorObject) {
-            console.log("Errors handled: " + errorObject.code);
-        });
 
-    });   /*added closing semicolons --- wenfang */
+                // World Weather Online apikey
+                $(".cityWeather").show();
+                $("#localTimeWeather").empty();
+                var wwoApiKey = 'ac4c2f19b48740fd9ce201023171209';
+                var weatherURL = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=" + cityName + "&format=json&key=" + wwoApiKey;
+                var tzURL = "http://api.worldweatheronline.com/premium/v1/tz.ashx?q=" + cityName + "&format=json&key=" + wwoApiKey;
+                $.ajax({
+                    url: tzURL,
+                    method: "GET"
+                }).done(function(localtime) {
+                    $("#localTimeWeather").prepend("Local Time:" + "\xa0\xa0\xa0\xa0" + localtime.data.time_zone[0].localtime + "<br/>");
+                });
 
-    //we need a button to go back to search
-    $(".cityImage").on("click", ".backbutton", function () {
-        location.reload();
-    })
+                $.ajax({
+                    url: weatherURL,
+                    method: "GET"
+                }).done(function(localweather) {
+                    $("#localTimeWeather").append("Current Weather: " + localweather.data.current_condition[0].weatherDesc[0].value +
+                        "\xa0\xa0\xa0\xa0\xa0\xa0" + "Temperature (F): " + localweather.data.current_condition[0].temp_F +
+                        "\xa0\xa0\xa0\xa0\xa0\xa0" + "Wind Speed (Mph): " + localweather.data.current_condition[0].windspeedMiles +
+                        "\xa0\xa0\xa0\xa0\xa0\xa0" + "Humidity: " + localweather.data.current_condition[0].humidity);
+                    var minTemp = "<tr><td>Avg Min Temp(F)<td>";
+                    var maxTemp = "<tr><td>Abs Max Temp(F)<td>";
+                    var rainFall = "<tr><td>Avg Daily Rainfall(in)<td>";
+                    for (var i = 0; i < 12; i++) {
+                        if (i === 11) {
+                            minTemp += localweather.data.ClimateAverages[0].month[i].avgMinTemp_F + "</td></tr>";
+                            maxTemp += localweather.data.ClimateAverages[0].month[i].absMaxTemp_F + "</td></tr>";
+                            rainFall += localweather.data.ClimateAverages[0].month[i].avgDailyRainfall + "</td></tr>";
+                        } else {
+                            minTemp += localweather.data.ClimateAverages[0].month[i].avgMinTemp_F + "</td><td>";
+                            maxTemp += localweather.data.ClimateAverages[0].month[i].absMaxTemp_F + "</td><td>";
+                            rainFall += localweather.data.ClimateAverages[0].month[i].avgDailyRainfall + "</td><td>";
+                        }
+                    }
+                    $("#weather-table > tbody").append(minTemp);
+                    $("#weather-table > tbody").append(maxTemp);
+                    $("#weather-table > tbody").append(rainFall);
+                });
 
-    //fetch most-searched places to the front page
-    var aRef = database.ref("Posts").orderByChild("srchCounter").limitToLast(3);
-    aRef.on("child_added", function(snapshot) {
-        var destHead = $("<h4>").text(snapshot.key);
-        var destDescription = $("<p>").text(snapshot.val()["Paragraph 1"]);
-        if(!srchResPage)
-            $(".popularSrches").append(destHead, destDescription);
-    });
+                /* Added codes for importing things-to-do contents to the cityToDo div -- wenfang */
+                $(".popularSrches").empty();
+                $(".cityToDo h2").text("Things To Do in " + cityName);
+                $(".cityToDo").append("<hr>");
+                // database.ref("Posts").on("value", function(snapshot) {
+                database.ref("Posts").once("value", function(snapshot) {
+                    if (snapshot.child(cityInput).exists()) {
+                        var childRef = snapshot.ref.child(cityInput);
+                        childRef.once("value", function(childsnap) {
+                            childsnap.forEach(function(snap) {
+                                var key = snap.key;
+                                if (key.includes("Paragraph")) {
+                                    var paragraphDiv = $("<div>").html("<p class='lead'>" + snap.val() + "</p>");
+                                    $(".cityToDo").append(paragraphDiv);
+                                } else if (key === "srchCounter") { //increment the city's search count
+                                    var cnt = snap.val() + 1;
+                                    childRef.child("srchCounter").set(cnt);
+                                }
+                            });
+                        });
+                    }
+                }, function(errorObject) {
+                    console.log("Errors handled: " + errorObject.code);
+                });
 
+            }); /*added closing semicolons --- wenfang */
+
+            //we need a button to go back to search
+            $(".cityImage").on("click", ".backbutton", function() {
+                location.reload();
+            })
+
+            //fetch most-searched places to the front page
+            var aRef = database.ref("Posts").orderByChild("srchCounter").limitToLast(3);
+            aRef.on("child_added", function(snapshot) {
+                var destHead = $("<h4>").text(snapshot.key);
+                var destDescription = $("<p>").text(snapshot.val()["Paragraph 1"]);
+                if (!srchResPage)
+                    $(".popularSrches").append(destHead, destDescription);
+            });
--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -1,56 +1,49 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDpNKmsnpL-vCHRFcrZjMfvqW9vG6Ilyjc",
-  authDomain: "travel-smart-928a0.firebaseapp.com",
-  databaseURL: "https://travel-smart-928a0.firebaseio.com",
-  projectId: "travel-smart-928a0",
-  storageBucket: "",
-  messagingSenderId: "244047018856"
+    apiKey: "AIzaSyDpNKmsnpL-vCHRFcrZjMfvqW9vG6Ilyjc",
+    authDomain: "travel-smart-928a0.firebaseapp.com",
+    databaseURL: "https://travel-smart-928a0.firebaseio.com",
+    projectId: "travel-smart-928a0",
+    storageBucket: "",
+    messagingSenderId: "244047018856"
 };
 firebase.initializeApp(config);
 
 // Create a variable to reference the database.
 var database = firebase.database()
 
-//displays modal
-// $('#myModal').modal('show');
-
 // Capture Button Click
-    $("#add-user").on("click", function(event) {
-      // prevent page from refreshing when form tries to submit itself
-      event.preventDefault();
+$("#add-user").on("click", function(event) {
+    // prevent page from refreshing when form tries to submit itself
+    event.preventDefault();
 
-      // Capture user inputs and store them into variables
-      var firstName = $("#firstname-input").val().trim();
-      var lastName = $("#lastname-input").val().trim();
-      var userName = $("#username-input").val().trim();
-      var email = $("#email-input").val().trim();
-      var password = $("#password-input").val().trim();
+    // Capture user inputs and store them into variables
+    var firstName = $("#firstname-input").val().trim();
+    var lastName = $("#lastname-input").val().trim();
+    var userName = $("#username-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var password = $("#password-input").val().trim();
 
-      // Console log each of the user inputs to confirm we are receiving them
-      console.log(firstName, lastName, userName, email, password);
-      
-      if((firstName=="") || (lastName=="") || (userName=="") || (email=="") || (password=="")) {
+    // Console log each of the user inputs to confirm we are receiving them
+    console.log(firstName, lastName, userName, email, password);
+
+    if ((firstName == "") || (lastName == "") || (userName == "") || (email == "") || (password == "")) {
 
         setError();
-             
-      }
 
-      else {
-          database.ref().push({
-              firstName: firstName,
-              lastName: lsatName,
-              userName: userName,
-              email: email,
-              password: password,
-              created: firebase.database.ServerValue.TIMESTAMP,
-              modified: firebase.database.ServerValue.TIMESTAMP
+    } else {
+        database.ref().push({
+            firstName: firstName,
+            lastName: lastName,
+            userName: userName,
+            email: email,
+            password: password,
+            created: firebase.database.ServerValue.TIMESTAMP,
+            modified: firebase.database.ServerValue.TIMESTAMP
 
-            });
-      }
+        });
+    }
 
-      $('input').val("");
+    $('input').val("");
 
-      });
-
-    };
+});
